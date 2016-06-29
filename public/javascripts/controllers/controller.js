8a85e4d37c793700ff853a092612f6eb15b585c8
James Freeman <freeman.james.h@gmail.com>
2016-06-29 10:57:01 -0600

    got carried away. 3 results for sample data

--- public/javascripts/controllers/controller.js
+++ public/javascripts/controllers/controller.js
@@ -32,27 +32,6 @@ app.controller('MainController', function($scope, $http){
       picture: 'http://images.craigslist.org/00c0c_izT4IZl7Q3n_300x300.jpg',
       price: '$50'
 
-    },
-    {
-      title: 'Thule 1050-09 Roof Rack Tray Upright Bicycle Carrier Single Bike NEW',
-      date:'Tue 28 Jun 02:34:36 PM',
-      picture: 'http://images.craigslist.org/00c0c_izT4IZl7Q3n_300x300.jpg',
-      price: '$50'
-
-    },
-    {
-      title: 'Thule 1050-09 Roof Rack Tray Upright Bicycle Carrier Single Bike NEW',
-      date:'Tue 28 Jun 02:34:36 PM',
-      picture: 'http://images.craigslist.org/00c0c_izT4IZl7Q3n_300x300.jpg',
-      price: '$50'
-
-    },
-    {
-      title: 'Thule 1050-09 Roof Rack Tray Upright Bicycle Carrier Single Bike NEW',
-      date:'Tue 28 Jun 02:34:36 PM',
-      picture: 'http://images.craigslist.org/00c0c_izT4IZl7Q3n_300x300.jpg',
-      price: '$50'
-
     }
   ]
 
