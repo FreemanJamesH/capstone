2d59dedb4f99986a05ae182ba0c638c5b78a5591
James Freeman <freeman.james.h@gmail.com>
2016-07-01 12:24:46 -0600

    hardcoded states...trying to cheerio them, now

--- craigscan/public/javascripts/controllers/controller.js
+++ craigscan/public/javascripts/controllers/controller.js
@@ -1,7 +1,62 @@
-app.controller('MainController', function($scope, $http, searchService, $location) {
+app.controller('MainController', function($scope, $http, searchService, stateListService, $location) {
   $scope.hello = 'hello'
   $scope.arr = searchService.resultsArrGetter();
-  console.log('ARR', $scope.arr);
+  stateListService.log()
+  $scope.stateList = [
+    ['Alabama', 'AL'],
+    ['Alaska', 'AK'],
+    ['Arizona', 'AZ'],
+    ['Arkansas', 'AR'],
+    ['California', 'CA'],
+    ['Colorado', 'CO'],
+    ['Connecticut', 'CT'],
+    ['Delaware', 'DE'],
+    ['Florida', 'FL'],
+    ['Georgia', 'GA'],
+    ['Hawaii', 'HI'],
+    ['Idaho', 'ID'],
+    ['Illinois', 'IL'],
+    ['Indiana', 'IN'],
+    ['Iowa', 'IA'],
+    ['Kansas', 'KS'],
+    ['Kentucky', 'KY'],
+    ['Louisiana', 'LA'],
+    ['Maine', 'ME'],
+    ['Maryland', 'MD'],
+    ['Massachusetts', 'MA'],
+    ['Michigan', 'MI'],
+    ['Minnessota', 'MN'],
+    ['Mississippi', 'MS'],
+    ['Missouri', 'MO'],
+    ['Montana', 'MT'],
+    ['Nebraska', 'NE'],
+    ['Nevada', 'NV'],
+    ['New Hampshire', 'NH'],
+    ['New Jersey', 'NJ'],
+    ['New Mexico', 'NM'],
+    ['New York', 'NY'],
+    ['North Carolina', 'NC'],
+    ['North Dakota', 'ND'],
+    ['Ohio', 'OH'],
+    ['Oklahoma', 'OK'],
+    ['Oregon', 'OR'],
+    ['Pennsylvania', 'PA'],
+    ['Rhode Island', 'RI'],
+    ['South Carolina', 'SC'],
+    ['South Dakota', 'SD'],
+    ['Tennessee', 'TN'],
+    ['Texas', 'TX'],
+    ['Utah', 'UT'],
+    ['Vermont', 'VT'],
+    ['Virginia', 'VA'],
+    ['Washington', 'WA'],
+    ['West Virginia', 'WV'],
+    ['Wisconsin', 'WI'],
+    ['Wyoming', 'WY']
+  ]
+
+  console.log($scope.stateList.length)
+
 
   $scope.submit = function() {
     var searchParams = {};
--- craigscan/public/javascripts/services.js
+++ craigscan/public/javascripts/services.js
@@ -20,3 +20,27 @@ app.service('searchService', ['$resource', function($resource) {
     }
   }
 }])
+
+
+app.service('stateListService', ['$resource', function($resource) {
+  var resultsArr = [];
+  return {
+    log: function() {
+      console.log('sls hello');
+    },
+    search: function(obj) {
+      console.log('serviceobj', obj)
+      return $resource('https://jhfcapstone.herokuapp.com/api').save({
+      // return $resource('http://localhost:3000/api').save({
+        sort: 'sort=rel',
+        query: 'query=' + obj.query
+      }).$promise.then(function(results) {
+        console.log('service results:', results)
+        resultsArr = results.dataArr;
+      })
+    },
+    resultsArrGetter: function() {
+      return resultsArr;
+    }
+  }
+}])
