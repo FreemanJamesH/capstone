8c8e3c466824b1fd0b7ef4cad9ae86bcc8d9c9bf
James Freeman <freeman.james.h@gmail.com>
2016-07-04 20:13:28 -0600

    heroku'd

--- craigscan/public/javascripts/services.js
+++ craigscan/public/javascripts/services.js
@@ -2,8 +2,8 @@ app.service('searchService', ['$resource', function($resource) {
   var resultsObj = {};
   return {
     search: function(obj) {
-      // return $resource('https://jhfcapstone.herokuapp.com/api').save({
-      return $resource('//localhost:3000/api').save({
+      return $resource('https://jhfcapstone.herokuapp.com/api').save({
+      // return $resource('//localhost:3000/api').save({
         url: obj.url
       }).$promise.then(function(results) {
         resultsObj = results;
@@ -21,7 +21,8 @@ app.service('stateListService', ['$resource', function($resource) {
   var resultsArr = [];
   return {
     retrieve: function(obj) {
-      return $resource('//localhost:3000/sls').get()
+      return $resource('https://jhfcapstone.herokuapp.com/sls').get()
+      // return $resource('//localhost:3000/sls').get()
       .$promise.then(function(results) {
         // console.log('results HERE', results)
         resultsArr = results.finish;
