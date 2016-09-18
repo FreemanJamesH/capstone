97438cb9329a42ed3b9e54b3ad6feff43e22a8e1
James Freeman <freeman.james.h@gmail.com>
2016-09-18 11:07:03 -0600

    using client-sessions instead of express-session

--- craigscan/server.js
+++ craigscan/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var session = require('express-session')
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
