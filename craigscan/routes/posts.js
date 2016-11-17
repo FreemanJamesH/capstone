053426045ffb8947e48a457557fc731c51d54b91
James Freeman <freeman.james.h@gmail.com>
2016-11-17 16:53:35 -0700

    differentiate posts, users, and search routes in server

---
+++ craigscan/routes/posts.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose')
+const request = require('request');
+const cheerio = require('cheerio');
+const User = mongoose.model('User')
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+
+// router.all('*', function(req, res, next) {
+//   console.log('WHOO');
+//   next()
+// })
+
+router.delete('/:searchid/:index', function(req, res, next) {
+
+  let searchId = req.params.searchid
+  console.log('searchid: ', searchId);
+  let resultIndex = req.params.index
+  let token = req.headers.token
+  jwt.verify(token, 'SECRET', function(err, decoded) {
+    if (err) {
+      return next(err)
+    } else {
+      User.findById(decoded._id, function(err, user) {
+        console.log('user found:', user);
+        for (var i = 0; i < user.searches.length; i++) {
+          if (user.searches[i].id === searchId) {
+            let deleted = user.searches[i].results.splice(resultIndex,1)
+            user.searches[i].results
+            console.log('spliced this:', deleted);
+            console.log('after splicing:', user.searches[i].results);
+          }
+        }
+        console.log('this is what Im saving: ', user.searches);
+        user.save(function(err, updatedUser) {
+          // console.log('updated user:', updatedUser);
+          res.json(updatedUser)
+        })
+      })
+    }
+  })
+})
+
+module.exports = router;
---
+++ craigscan/routes/search.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose')
+const request = require('request');
+const cheerio = require('cheerio');
+const User = mongoose.model('User')
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+router.get('/dashboard', function(req, res, next) {
+  if (!req.headers.token) {
+    let err = new Error()
+    err.message = 'You are not logged in.'
+    err.status = 403
+    return next(err)
+  } else {
+    let token = req.headers.token
+    jwt.verify(token, 'SECRET', function(err, decoded) {
+      if (err) {
+        return next(err)
+      } else {
+        User.findById(decoded._id, function(err, user) {
+          res.send(user)
+        })
+      }
+    })
+  }
+})
+
+router.post('/savesearch', function(req, res, next) {
+  if (!req.headers.token) {
+    let err = new Error()
+    err.message = 'You are not logged in.'
+    err.status = 403
+    return next(err)
+  } else {
+    let token = req.headers.token
+    jwt.verify(token, 'SECRET', function(err, decoded) {
+      if (err) {
+        return next(err)
+      } else {
+        User.findById(decoded._id, function(err, user) {
+          user.searches.push(req.body)
+          user.save(function(err, updatedUser) {
+            console.log(updatedUser);
+          })
+        })
+      }
+    })
+  }
+})
+
+router.post('/deletesearch', function(req, res, next) {
+  let idToDelete = req.body.id
+  let token = req.headers.token
+  jwt.verify(token, 'SECRET', function(err, decoded) {
+    if (err) {
+      return next(err)
+    } else {
+      User.findById(decoded._id, function(err, user) {
+        for (var i = 0; i < user.searches.length; i++) {
+          if (user.searches[i].id === idToDelete) {
+            user.searches.splice(i, 1)
+            i--
+          }
+        }
+        user.save(function(err, updatedUser) {
+          res.json(updatedUser)
+        })
+      })
+    }
+  })
+})
+
+router.get('/getsearch/:id', function(req, res, next) {
+  let idToGet = req.params.id
+  let token = req.headers.token
+  jwt.verify(token, 'SECRET', function(err, decoded) {
+    if (err) {
+      return next(err)
+    } else {
+      User.findById(decoded._id, function(err, user) {
+        for (var i = 0; i < user.searches.length; i++) {
+          if (user.searches[i].id === idToGet) {
+            console.log('retrieved search info: ', {search: user.searches[i]});
+            return res.json(user.searches[i])
+          }
+        }
+      })
+    }
+  })
+})
+
+
+
+
+module.exports = router;
