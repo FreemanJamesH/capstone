c4b78627f5678f8139815b74bc5cdbb132442372
James Freeman <freeman.james.h@gmail.com>
2017-02-25 16:33:16 -0700

    dashboard colors don't shift when search is deleted

--- craigscan/public/javascripts/controllers/dashCtrl.js
+++ craigscan/public/javascripts/controllers/dashCtrl.js
@@ -3,14 +3,27 @@ app.controller('dashController', function($scope, $location, userService, search
   $scope.updating = false;
 
   userService.getUser().then(function(results) {
+    $scope.colorTracker = []
+    for (var i = 0; i < results.searches.length; i++) {
+      $scope.colorTracker[i] = 0
+    }
+    console.log($scope.colorTracker);
     $scope.userObj = results
     console.log($scope.userObj);
   })
 
-  $scope.deleteSearch = function(id) {
+  $scope.deleteSearch = function(id, index) {
+
     searchService.deleteSearch(id).then(function(results) {
+      $scope.colorTracker.splice(index,1)
+      for (var i = index; i < $scope.colorTracker.length; i++){
+        $scope.colorTracker[i]++
+      }
       $scope.userObj = results
     })
+
+    console.log(`searches in user obj:`, $scope.userObj.searches);
+
   }
 
   $scope.viewSearch = function(id) {
